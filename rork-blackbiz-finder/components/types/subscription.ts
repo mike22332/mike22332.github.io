9812ed0ebaf6b6cee508c7c0143ef40eefea652b
mike22332 <michaelg.bllt@gmail.com>
2025-06-17 14:19:49 -0400

    Delete rork-blackbiz-finder directory

--- rork-blackbiz-finder/components/types/subscription.ts
+++
@@ -1,9 +0,0 @@
-export type SubscriptionTierId = 'basic' | 'standard' | 'premium';
-
-export interface SubscriptionTier {
-  id: SubscriptionTierId;
-  name: string;
-  price: number;
-  features: string[];
-  color: string;
-}
